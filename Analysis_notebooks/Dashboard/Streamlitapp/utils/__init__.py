2ded10b0b25ec76cd2783c6eebf17f30844b1588
birhanumoges <birhanumoges726@gmail.com>
2026-03-25 16:32:47 -0300

    commit

--- Analysis_notebooks/Dashboard/Streamlitapp/utils/__init__.py
+++ Analysis_notebooks/Dashboard/Streamlitapp/utils/__init__.py
@@ -3,6 +3,9 @@ Utils package for Ethiopian Student Performance Dashboard
 r"C:/Users/DELL/Documents/project_data/ethiopian_students_dataset.csv"
 """
 
-from .data_processor import *
-from .visualizations import *
-from .predictions import *
+# utils/__init__.py
+from .data_processor import DataProcessor
+from .predictions import PredictionEngine
+from .visualizations import Visualizer
+
+__all__ = ['DataProcessor', 'PredictionEngine', 'Visualizer']
