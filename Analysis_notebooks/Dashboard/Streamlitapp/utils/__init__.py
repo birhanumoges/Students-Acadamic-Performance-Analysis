b2ea664f473317b2e0f7b65605437d8c80d5cd20
birhanumoges <birhanumoges726@gmail.com>
2026-03-27 16:38:52 -0300

    commit

--- Analysis_notebooks/Dashboard/Streamlitapp/utils/__init__.py
+++ Analysis_notebooks/Dashboard/Streamlitapp/utils/__init__.py
@@ -1,11 +1,45 @@
 """
 Utils package for Ethiopian Student Performance Dashboard
-r"C:/Users/DELL/Documents/project_data/ethiopian_students_dataset.csv"
 """
 
-# utils/__init__.py
-from .data_processor import DataProcessor
-from .predictions import PredictionEngine
-from .visualizations import Visualizer
+from .data_processor import (
+    DataProcessor,
+    COLOR_SCHEME,
+    load_and_preprocess_data,
+    encode_categorical_features,
+    prepare_target_encoders,
+    process_raw_input_for_prediction
+)
 
-__all__ = ['DataProcessor', 'PredictionEngine', 'Visualizer']
+from .visualizations import (
+    Visualizer,
+    initialize_global_vars,
+    set_global_data,
+    set_prediction_result
+)
+
+from .predictions import (
+    PredictionEngine,
+    load_models,
+    make_prediction_corrected,
+    get_national_exam_model_performance,
+    get_national_exam_feature_importance
+)
+
+__all__ = [
+    'DataProcessor',
+    'PredictionEngine',
+    'Visualizer',
+    'COLOR_SCHEME',
+    'load_and_preprocess_data',
+    'encode_categorical_features',
+    'prepare_target_encoders',
+    'process_raw_input_for_prediction',
+    'initialize_global_vars',
+    'set_global_data',
+    'set_prediction_result',
+    'load_models',
+    'make_prediction_corrected',
+    'get_national_exam_model_performance',
+    'get_national_exam_feature_importance'
+]
